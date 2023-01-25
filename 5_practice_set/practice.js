2c0c9d2e610552c59ad097de2adeb7562d51c502
AkashRuidas <akash17aeccse@gmail.com>
2023-01-25 10:41:05 +0530

    Practice Set

---
+++ 5_practice_set/practice.js
@@ -0,0 +1,25 @@
+//1.Create a variable of type string and try to add a number to it.
+
+let str = "Hello";
+str += 5;
+console.log(str);
+console.log(typeof (str));
+
+
+//2.Create a object of dictionary meaning of word
+
+let dict = {
+    "Hello": "Greeting",
+    "World": "Earth",
+    "Earth": "Planet",
+    "How": "Question",
+    "Are": "Question"
+}
+//print the object in console
+console.log(dict);
+
+//print the type of the object
+console.log(typeof (dict));
+
+
+
