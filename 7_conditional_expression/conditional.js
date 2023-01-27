2ab025ba9aba5cd9978f598292df06cf7ecc4048
AkashRuidas <akash17aeccse@gmail.com>
2023-01-27 09:41:16 +0530

    Added(): Js Basics

---
+++ 7_conditional_expression/conditional.js
@@ -0,0 +1,24 @@
+//if statement in JavaScript
+let a = 10;
+a=Number.parseInt(a);
+if (a > 5) {
+    console.log("a is greater than 5");
+}
+
+//if else statement in JavaScript
+let b = 20;
+if (a > 5) {
+    console.log("a is greater than 5");
+}
+else {
+    console.log("a is less than 5");
+}
+
+//if else if statement in JavaScript
+let c = 30;
+if (a > 5) {
+    console.log("a is greater than 5");
+}
+else if (a < 5) {
+    console.log("a is less than 5");
+}
---
+++ 7_conditional_expression/ternary.js
@@ -0,0 +1,3 @@
+//ternary operator in js
+let a =10;
+a>5?console.log("a is greater than 5"):console.log("a is less than 5");
